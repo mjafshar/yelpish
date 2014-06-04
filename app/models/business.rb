b85106503e6b9d10ea5ffc00bbb938ec6b121644
Javad Afshar <mjafshar@gmail.com>
2014-06-04 15:33:50 -0500

    These edits fix the issue where I couldn't get info out of the DB properly. Turns out my model was trying to do too much.
    Removed all code from model allowing CDQ to handle attrs
    
    Changed the BUSINESS_PROPERTIES with the correct attrs needed from the API return
    
    Fixed schema file with correct data types
    
    Updated the Xcode data models accordingly

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,17 +1,3 @@
 class Business < CDQManagedObject
-  BUSINESS_PROPERTIES.each { |prop|
-    attr_accessor prop
-  }
 
-  def initialize(hash = {})
-    hash.each { |key, value|
-      if self.respond_to?(key.to_s + "=")
-        self.send((key.to_s + "=").to_s, value)
-      end
-    }
-  end
-
-  def categories=(new_categories_list)
-    self.category = new_categories_list.first[:name]
-  end
 end
