13dd2ccd4069a3a425465409760a115657e39893
Javad Afshar <mjafshar@gmail.com>
2014-06-12 19:05:45 -0500

    Updated to initialize with business object instead of address
    Moved initialization of MKMapView into loadView method
    
    Added business title to map pin
    
    Cleaned up test code

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,55 +1,45 @@
 class MapController < UIViewController
-  attr_accessor :address
+  attr_accessor :business
 
-  def viewDidLoad
-    get_location_from_address(address)
+  def loadView
+    super
+    self.view = MKMapView.alloc.init
+    self.view.zoomEnabled = true
+    self.view.scrollEnabled = true
+    view.delegate = self
   end
 
-  def initWithAddress(address)
+  def viewDidLoad
+    get_location_from_address(business.address)
+  end
+
+  def initWithBusiness(business)
     init
-    self.address = address
+    self.business = business
     self
   end
 
   def get_location_from_address(address)
-    # address = address.address
-    NSLog("Address: #{address}")
-    map_view = MKMapView.alloc.init
-    map_view.frame = self.view.frame
-    map_view.delegate = self
-    # map_view.region = CoordinateRegion.new([41.889911, -87.637657], [0.2, 0.2])
-    map_view.showsUserLocation = true
-    map_view.zoomEnabled = true
-    map_view.scrollEnabled = true
-
     geo_coder = CLGeocoder.alloc.init
-
+    NSLog("Address: #{address}")
     geo_coder.geocodeAddressString(address, completionHandler:lambda do |coord, error|
-        if coord
-          NSLog("Coord: #{coord.first.location.coordinate.latitude}")
-          NSLog("Coord: #{coord.first.location.coordinate.longitude}")
-          NSLog("Coord: #{coord.first.location.coordinate}")
-          lat = coord.first.location.coordinate.latitude
-          long = coord.first.location.coordinate.longitude
-          # coord = {latitude: lat, longitude: long}
-          # placemarker = MKPlacemark.alloc.initWithPlacemark(coord.first)
-          coords = coord.first.location.coordinate
-          placemarker = MKPointAnnotation.new
-          placemarker.coordinate = coords
+      if coord
+        coords = coord.first.location.coordinate
+        placemarker = MKPointAnnotation.new
+        placemarker.coordinate = coords
+        placemarker.title = business.name
 
-          region = MKCoordinateRegion.new
-          region.center = coords
+        region = MKCoordinateRegion.new
+        region.center = coords
 
-          map_view.region = region
-
-          map_view.addAnnotation(placemarker)
-        else
-          NSLog("Error: #{error.to_s}")
-          coord = error
-        end
-        self.view.addSubview(map_view)
-        # self.view.reloadData
+        self.view.region = region
+        self.view.addAnnotation(placemarker)
+      else
+        NSLog("Error Description: #{error.localizedDescription}")
+        NSLog("Failure Reason: #{error.localizedFailureReason}")
+        NSLog("Recovery Suggestion: #{error.localizedRecoverySuggestion}")
+        error
       end
-    )
+    end)
   end
 end
